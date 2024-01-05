22cb4f3891b3bfa4e40481aeacc4a923977c1de3
soffi49 <zosia.wrona49@gmail.com>
2024-01-05 21:36:09 +0100

    Implemented hungarian

--- src/models/graph.py
+++ src/models/graph.py
@@ -1,8 +1,62 @@
 import numpy as np
 
 from typing import List
-from src.models.node import Node
-from src.models.edge import Edge
+
+class Node():
+    '''
+    Class represents single graph node.
+    '''
+
+    def __init__(self, 
+                 coordinates: np.ndarray, 
+                 idx: int,
+                 label: float = 0) -> None:
+        '''
+        Parameters:
+        coordinates - x and y coordinates of a point
+        idx - index of the node in the list of nodes in graph
+        label - label assigned to the node (by default 0)
+        '''
+        self.idx = idx
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+        self.label = label
+        self.edges: List[Edge] = []
+
+    def compute_distance(self, node) -> float:
+        '''
+        Method computes distance between two points.
+
+        Parameters:
+        node - node of a point with which the distance is to be computed
+
+        Return: euclidean distance between two points
+        '''
+        return np.linalg.norm([self.x, self.y], [node.x, node.y])
+    
+    def get_weights_of_edges(self) -> List[float]:
+        '''
+        Method returns a list of weights of all edges that go from the given node.
+
+        Returns: list of edges' weights
+        '''
+        return [edge.weight for edge in self.edges]
+
+class Edge():
+    '''
+    Class describing edge in the graph.
+    '''
+
+    def __init__(self, src: Node, dst: Node, weight: float) -> None:
+        '''
+        Parameters:
+        src - source node
+        dst - destination done
+        weight - weight of the edge
+        '''
+        self.src = src
+        self.dst = dst
+        self.weight = weight
 
 class Graph():
     '''
@@ -66,6 +120,7 @@ class Graph():
                 edge = Edge(house, well, -distance)
                 edges.append(edge)
                 house.edges.append(edge)
+                well.edges.append(edge)
         return edges
     
     def duplicate_wells(self):
